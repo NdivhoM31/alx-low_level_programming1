8a48822e469ab46ed8b34931449ea92b43439eaf
NdivhoM31 <ndivhomufhadi8@gmail.com>
2023-03-16 17:04:16 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
+ * main - prints the phrase with proper grammer,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	Return(0);
+	return (0);
 }
